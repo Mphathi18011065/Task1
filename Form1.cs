795974f919f61fa8d306c309c7c6f8ebd27d0619
Mphathi18011065 <18011065@vegaconnnect.co.za>
2019-10-07 16:05:25 +0200

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task1
+{
+    public partial class Form1 : Form
+    {
+        GameEngine engine;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            engine = new GameEngine(20, txtInfo, grpMap);
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+
+        private void BtnPause_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            lblRound.Text = "Round: " + engine.Round.ToString();
+            engine.Update();
+        }
+    }
+}
